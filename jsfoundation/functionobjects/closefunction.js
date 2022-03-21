29f31ac8a19865a3e99d08c37d6cb5400103daaf
Vanshika <vanshika@Vanshikas-MacBook-Pro.local>
2022-03-21 12:23:53 +0530

    Added changes for compionents

--- jsfoundation/functionobjects/closefunction.js
+++ jsfoundation/functionobjects/closefunction.js
@@ -1,16 +1,24 @@
 // Create a CLose FUnction or a function object that will return the Literal with public members in it
 
-function Logic(){
-    var employees = [];
-    employees.push({EmpNo:101, EmpName:'A'});
-    employees.push({EmpNo:102, EmpName:'B'});
-    return {
-        getEmployees:function(){
-            return employees;
-        },
-        addEmployee:function(emp){
-            employees.push(emp);
-            return employees;
-        }
-    };
+function Logic() {
+  var employees = [];
+  employees.push({ EmpNo: 101, EmpName: "A" });
+  employees.push({ EmpNo: 102, EmpName: "B" });
+  return {
+    getEmployees: function () {
+      return employees;
+    },
+    addEmployee: function (emp) {
+      employees.push(emp);
+      return employees;
+    },
+    deleteEmployee: function (emp) {
+      employees.pop(emp);
+      return employees;
+    },
+    updateEmployee: function (emp) {
+      employees[1].EmpNo = emp[0].EmpNo;
+      return employees;
+    },
+  };
 }
--- jsfoundation/functionobjects/uigenerator.js
+++ jsfoundation/functionobjects/uigenerator.js
@@ -1,21 +1,92 @@
 // the Constructor function accepts a dataSource
-// this will be a collection 
-var UIGenerator = function(dataSource){
-    // this function wil be used to generate a
-    // HTML <select> element
-    this.select = function(){
-        
-        // check if the data is passed to generate the UI
-        if(dataSource === undefined || dataSource.length === 0){
-            return 'No Data So No UI will be generated'    
-        } else {
-            var select = '<select>';
-            // iterate over the dataSource and Generate a UI
-            for(var i=0;i<dataSource.length;i++){
-                 select+= '<option value="'+ dataSource[i] +'">'+ dataSource[i] + '</options>';   
-            }  
-            select += '</select>';
-            return select;
-        }
-    };
-}
+// this will be a collection
+var UIGenerator = function (dataSource) {
+  // this function wil be used to generate a
+  // HTML <select> element
+  this.select = function () {
+    // check if the data is passed to generate the UI
+    if (dataSource === undefined || dataSource.length === 0) {
+      return "No Data So No UI will be generated";
+    } else {
+      var select = "<select>";
+      // iterate over the dataSource and Generate a UI
+      for (var i = 0; i < dataSource.length; i++) {
+        select +=
+          '<option value="' +
+          dataSource[i] +
+          '">' +
+          dataSource[i] +
+          "</options>";
+      }
+      select += "</select>";
+      return select;
+    }
+  };
+};
+var UIGenerator2 = function (dataSource) {
+  // this function wil be used to generate a
+  // HTML <select> element
+  console.log("employeede", dataSource);
+  this.select = function () {
+    // check if the data is passed to generate the UI
+    if (dataSource === undefined || dataSource.length === 0) {
+      return "No Data So No UI will be generated";
+    } else {
+      var select = "<select>";
+      // iterate over the dataSource and Generate a UI
+      for (var i = 0; i < dataSource.length; i++) {
+        select +=
+          '<option value="' +
+          dataSource[i].designation +
+          '">' +
+          dataSource[i].designation +
+          "</options>";
+      }
+      select += "</select>";
+      return select;
+    }
+  };
+};
+var UIGenerator3 = function (dataSource) {
+  // this function wil be used to generate a
+  // HTML <select> element
+  console.log("employeede", dataSource);
+  this.select = function () {
+    // check if the data is passed to generate the UI
+    if (dataSource === undefined || dataSource.length === 0) {
+      return "No Data So No UI will be generated";
+    } else {
+      var select = "<select>";
+      // iterate over the dataSource and Generate a UI
+      for (var i = 0; i < dataSource.length; i++) {
+        select +=
+          '<option value="' +
+          dataSource[i].deptName +
+          '">' +
+          dataSource[i].deptName +
+          "</options>";
+      }
+      select += "</select>";
+      return select;
+    }
+  };
+};
+var UIGenerator1 = function (dataSource) {
+  // this function wil be used to generate a
+  // HTML <select> element
+  this.select = function () {
+    // check if the data is passed to generate the UI
+    if (dataSource === undefined || dataSource.length === 0) {
+      return "No Data So No UI will be generated";
+    } else {
+      var select = "<table>";
+      // iterate over the dataSource and Generate a UI
+      for (var i = 0; i < dataSource.length; i++) {
+        select +=
+          '<tr value="' + dataSource[i] + '">' + dataSource[i] + "</tr>";
+      }
+      select += "</table>";
+      return select;
+    }
+  };
+};
