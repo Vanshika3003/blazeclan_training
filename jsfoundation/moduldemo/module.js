495a9a702da10fb26580690ffafeaedb0a0d3283
maheshsabnis <sabnis_m@hotmail.com>
2022-03-21 17:16:54 +0530

    Date:22-March-2022

---
+++ jsfoundation/moduldemo/module.js
@@ -0,0 +1,8 @@
+// global to module.js
+// var i = 10;
+// Immediately Invocable Function Expression 
+(function (){
+    var i = 10;
+    console.log("In The IIFE i = " + i);
+    alert("Call ");
+})();
---
+++ jsfoundation/moduldemo/module1.js
@@ -0,0 +1,1 @@
+console.log('in module1.js i = '+ i);
